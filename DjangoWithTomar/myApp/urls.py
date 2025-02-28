0b30495ceb51c3914a0aee2ff2fea3d0c8d3e5de
Sujit Tomar <tomarcodinglife@gmail.com>
2025-02-28 19:31:48 +0530

    Delete DjangoWithTomar directory

--- DjangoWithTomar/myApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path ('', views.first_Page, name='First Page'),
-    
-    path ('sujit', views.sujit, name='sujit'),
-]
--- DjangoWithTomar/myApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def first_Page(request) :
-    return render (request, 'myApp/first_Page.html')
-
-def sujit(request) :
-    return render (request, 'myApp/sujit.html')
-
